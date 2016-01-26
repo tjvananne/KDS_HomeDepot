a0e2bc72ce2f68d1ba9f5655a5a5c7bef19f36d1
Taylor Van Anne <Taylor Van Anne>
2016-01-25 19:01:22 -0600

    Commit!

--- benchmark-score-script.R
+++ benchmark-score-script.R
@@ -1,25 +1,38 @@
 
-setwd("~/Analytics/Kaggle/HomeDepot")
+#setwd("~/Analytics/Kaggle/HomeDepot")
+setwd("C:/Users/tjvan/Documents/Kaggle/HomeDepot_early_2016/homedepotgit")
 #https://www.kaggle.com/dsoreo/home-depot-product-search-relevance/benchmark-score-script
 
-
+#install.packages("readr")
 library(readr)
+library(data.table)
+library(dplyr)
 cat("Reading data\n")
-train <- read_csv('input/train.csv')
-test <- read_csv('input/test.csv')
-desc <- read_csv('input/product_descriptions.csv')
+train <- read_csv('../input/train.csv')
+test <- read_csv('../input/test.csv')
+desc <- read_csv('../input/product_descriptions.csv')
+attributes <- read_csv('../input/attributes.csv')
+brandnames <- filter(attributes, name == "MFG Brand Name") 
+colnames(brandnames) <- c("product_uid", "name", "brandname")
+
+rm(attributes)
+
 
 cat("Merge description with train and test data \n")
 train <- merge(train,desc, by.x = "product_uid", by.y = "product_uid", all.x = TRUE, all.y = FALSE)
-train2 <- train[1:20, ]
 test <- merge(test,desc, by.x = "product_uid", by.y = "product_uid", all.x = TRUE, all.y = FALSE)
+
+cat("Merge the brand name with train and test data \n")
+train <- merge(train,brandnames, by.x = "product_uid", by.y = "product_uid", all.x = TRUE, all.y = FALSE); train$name = NULL
+test <- merge(test,brandnames, by.x = "product_uid", by.y = "product_uid", all.x = TRUE, all.y = FALSE); test$name = NULL
 
 t <- Sys.time()
 #this function takes a search, splits those words up, then attempts to find each of those words in the product title and description
 #output is a how many words were matched in the title, how many words were there total in search phrase, how many words matched the description
-word_match <- function(words,title,desc){
+word_match <- function(words,title,desc,brand){
        n_title <- 0
        n_desc <- 0
+       n_brand <- 0
        #print(words)  #mine
        #print(title)  #mine
        #print(desc)   #mine
@@ -29,24 +42,35 @@ word_match <- function(words,title,desc){
               pattern <- paste("(^| )",words[i],"($| )",sep="")
               n_title <- n_title + grepl(pattern,title,perl=TRUE,ignore.case=TRUE)
               n_desc <- n_desc + grepl(pattern,desc,perl=TRUE,ignore.case=TRUE)
+              n_brand <- n_brand + grepl(pattern,brand,perl=TRUE,ignore.case=TRUE)
        }
-       return(c(n_title,nwords,n_desc))
+       return(c(n_title,nwords,n_desc, n_brand))
 }
 
 cat("Get number of words and word matching title in train\n")
-train_words <- as.data.frame(t(mapply(word_match,train$search_term,train$product_title,train$product_description)))
+train_words <- as.data.frame(t(mapply(word_match,train$search_term,train$product_title,train$product_description,train$brandname)))
 #train2_words <- as.data.frame(t(mapply(word_match, train2$search_term, train2$product_title, train2$product_description))) #mine
 train$nmatch_title <- train_words[,1]
 train$nwords <- train_words[,2]
 train$nmatch_desc <- train_words[,3]
+train$nmatch_brand <- train_words[,4]
 
 cat("Get number of words and word matching title in test\n")
-test_words <- as.data.frame(t(mapply(word_match,test$search_term,test$product_title,test$product_description)))
+test_words <- as.data.frame(t(mapply(word_match,test$search_term,test$product_title,test$product_description,test$brandname)))
 test$nmatch_title <- test_words[,1]
 test$nwords <- test_words[,2]
 test$nmatch_desc <- test_words[,3]
+test$nmatch_brand <- test_words[,4]
 
 rm(train_words,test_words)
+
+write_csv(test, "../output/hdp_testwbrand.csv")
+write_csv(train, "../output/hdp_trainwbrand.csv")
+
+
+# ********* Modeling begins here - moving that function to Azure ML ***************
+
+
 
 cat("A simple linear model on number of words and number of words that match\n")
 glm_model <- glm(relevance~nmatch_title+nmatch_desc+nwords,data=train)
@@ -63,12 +87,23 @@ test_relevance
 test_relevance <- ifelse(test_relevance>3,3,test_relevance)
 test_relevance <- ifelse(test_relevance<1,1,test_relevance)
 
+head(test_relevance)
 
+#this is for C5
+for (i in 1:nrow(test)) {
+    if (test[i, 10] > 0) {
+        test_relevance[i] <- test_relevance[i] * 1.07
+    }
+}
+
+test_relevance <- ifelse(test_relevance>3,3,test_relevance)
 
 summary(test_relevance)
 
-submission <- data.frame(id=test$id,relevance=test_relevance)
-write_csv(submission,"benchmark_submission.csv")
+test$test_relevance <- test_relevance
+
+submission_C9 <- data.frame(id=test$id,relevance=test_relevance)
+write_csv(submission_C7,"../output/benchmark_submission_C9.csv")
 print(Sys.time()-t)
 
 #*****************MINE AGAIN ***************************************
@@ -105,6 +140,15 @@ library(dplyr)
 library(data.table)
 
 
+names(train)
+withbrandmatch <- filter(train, nmatch_brand > 0)
+withoutbrandmatch <- filter(train, nmatch_brand == 0)
+
+mean(withbrandmatch$relevance)
+summary(withbrandmatch$relevance)
+
+mean(withoutbrandmatch$relevance)
+summary(withoutbrandmatch$relevance)
 
 names(train)
 
